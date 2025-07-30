9342b1faea99e5cdd40f81f583f702699e3cf5dc
Mayank Ingole <mayankingole17may@gmail.com>
2025-07-30 22:59:05 +0530

    Update live_Class.py

--- live_Class.py
+++ live_Class.py
@@ -1,5 +1,2 @@
-n1 = (10, 20, 30, 40, 50)
-n1_list = list(n1)
-n1_list[0], n1_list[-1] = n1_list[-1], n1_list[0]
-n1 = tuple(n1_list)
-print("The swapped tuple is ", n1)
+n1 = (1, 2, 3, 4, 5, 6, 7)
+print(n1[:8])
