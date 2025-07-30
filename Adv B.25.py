c2c929a6749472e778feaa6d94fa67a24ad222d8
Mayank Ingole <mayankingole17may@gmail.com>
2025-07-30 23:37:31 +0530

    Create Adv B.25.py

---
+++ Adv B.25.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+if num**2 > 1000:
+    print("The square of the number is greater than 1000")
+else: 
+    print("The square of the number is not greater than 1000")
