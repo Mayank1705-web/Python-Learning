69f80c056b7b6085ab3f27256663475204599a8b
Mayank Ingole <mayankingole17may@gmail.com>
2025-07-30 23:36:30 +0530

    Create Adv A.8.py

---
+++ Adv A.8.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+if num1 % num2 == 0:
+    print("The number is multiple of other.")
+else: 
+    print("The number is not multiple of other.")
