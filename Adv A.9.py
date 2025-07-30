098de4d656123582d97eab0e7c7576f6a5930da7
Mayank Ingole <mayankingole17may@gmail.com>
2025-07-30 23:36:33 +0530

    Create Adv A.9.py

---
+++ Adv A.9.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+if num1 > num2:
+    print("The number ", num1," is greatest number.")
+elif num2 > num1:
+    print("The number ", num2," is greatest number.")
+else:
+    print("Both the number are equals.")
